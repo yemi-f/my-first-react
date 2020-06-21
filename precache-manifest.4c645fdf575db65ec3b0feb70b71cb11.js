bb6812f4772ec1e798b8fb58495a127181037a10
Yemi Fakorede <yemifakorede@gmail.com>
2020-06-21 07:44:34 -0400

    Updates

---
+++ precache-manifest.4c645fdf575db65ec3b0feb70b71cb11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feb8662b08970dfc27a828b78429432",
+    "url": "/my-first-react/index.html"
+  },
+  {
+    "revision": "540b462e9a7cf2a53aaf",
+    "url": "/my-first-react/static/css/main.0d9734e9.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/my-first-react/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540b462e9a7cf2a53aaf",
+    "url": "/my-first-react/static/js/main.08d23ebd.chunk.js"
+  },
+  {
+    "revision": "f2733162b94d76dea2fc",
+    "url": "/my-first-react/static/js/runtime-main.86b2d65e.js"
+  }
+]);
